5f1511d97c0acf33b175163c80d43f94804198d7
nhat thang <nhtthng2812@gmail.com>
2025-02-10 22:39:27 +0700

    add selectedItem 'push route'

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/models/category.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
-
+import 'package:meal_app/screens/meals.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _SelectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        )
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +35,9 @@ class CategoriesScreen extends StatelessWidget {
           children: [
             // vòng for này thay thế cho map như sau: availableCategories.map((category) => CategoryGridItem(category: category)).toList()
             for (final category in availableCategories)
-              CategoryGridItem(category: category)
+              CategoryGridItem(category: category, onSelectCategory: () {
+                _SelectCategory(context, category);
+              })
           ],
       ),
     );
