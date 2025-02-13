0d7983607f97f2987963f31fbabbf7cc05240632
nhat thang <nhtthng2812@gmail.com>
2025-02-13 10:40:24 +0700

    improve mealDetail

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -12,12 +12,40 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
-        ),
+      body:
+       SingleChildScrollView(
+         child: Column( // cần wrap column này trong singleChildScrollView để tránh trường hợp bị overflow hoặc sử dụng ListView thay vì Column
+          children: [
+            Image.network(
+          meal.imageUrl,
+          height: 300,
+          width: double.infinity,
+          fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14,),
+           Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+           ),),
+           const SizedBox(height: 14,),
+           for (final ingredient in meal.ingredients)
+            Text(ingredient,style: TextStyle(color: Colors.white),),
+         
+            const SizedBox(height: 24,),
+            Text('Steps', style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
+            const SizedBox(height: 14,),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
+                child: Text(step,style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+              ),
+          ]
+         ),
+       )
+       
     );
   }
 }
